0fc85c6b78cb825edfd777ddc9a896b3d132591d
Juan Fernando Barberi <157664356+jbarberi@users.noreply.github.com>
2024-10-18 21:42:30 -0500

    Model9(CART) - JuanF

--- Problem Set 2/scripts/solo/Juan F/script/JuanF2.R
+++ Problem Set 2/scripts/solo/Juan F/script/JuanF2.R
@@ -48,7 +48,7 @@ print(model3)
 # Kaggle modelo 3:
 
 predictSample <- test   %>% 
-  mutate(pobre_lab = predict(model1, newdata = test, type = "raw")    ## predicted class labels
+  mutate(pobre_lab = predict(model3, newdata = test, type = "raw")    ## predicted class labels
   )  %>% select(id,pobre_lab)
 
 head(predictSample)
@@ -64,6 +64,58 @@ name<- paste0("scripts/solo/Juan F/submisison/","RF_SMOTE_mtry", "15", ".csv")
 write.csv(predictSample,name, row.names = FALSE)
 
 
+
+###Modelo 5 CART:
+
+# Cargar la librería necesaria
+library(caret)
+
+ctrl <- trainControl(
+  method = "cv",
+  number = 5,
+  classProbs = TRUE,           
+  summaryFunction = prSummary, 
+  savePredictions = TRUE,      
+  verboseIter = TRUE,          
+  sampling = "smote"           
+)
+
+
+set.seed(098063)
+
+model9 <- train(
+  poor ~ .,                   # Variable objetivo y predictoras
+  data = train,                # Conjunto de datos de entrenamiento
+  method = "rpart",            # Usar CART (árboles de decisión)
+  metric = "F",               # Usar F1 como métrica de evaluación
+  trControl = ctrl,            # Control del modelo (validación cruzada con SMOTE)
+  tuneLength = 10,             # Ajustar el número de hiperparámetros a explorar
+  parms = list(split = "information") # Ajustar el criterio de división a "information gain"
+)
+
+# Mostrar los resultados del modelo
+print(model9)
+
+
+
+
+# Kaggle modelo 9:
+
+predictSample <- test   %>% 
+  mutate(pobre_lab = predict(model9, newdata = test, type = "raw")    ## predicted class labels
+  )  %>% select(id,pobre_lab)
+
+head(predictSample)
+
+
+predictSample<- predictSample %>% 
+  mutate(pobre=ifelse(pobre_lab=="Yes",1,0)) %>% 
+  select(id,pobre)
+head(predictSample)  
+
+name<- paste0("scripts/solo/Juan F/submisison/","CART_cp", "00013", ".csv") 
+
+write.csv(predictSample,name, row.names = FALSE)
 
 
 
