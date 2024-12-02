ccd257db3b842ef4fbb34e2a20e64e2f5cb460df
mafeblanco19 <m.blancoc@uniandes.edu.co>
2024-12-01 23:38:51 -0500

    set data

---
+++ Problem Set 3/scripts/02_data_location.R
@@ -0,0 +1,66 @@
+# ============================================================================ #
+# Problem Set 1                                                                #
+# Big Data and Machine Learning - 202420                                       #
+#                                                                              #
+# Script: Location Variables                                                   #
+#                                                                              #
+# Team 1: - Sergio Sandoval                                                    #
+#         - María Fernanda Blanco                                              #
+#         - Juan Fernando Barberi                                              #
+#         - Juan Gutierrez                                                     #
+# ============================================================================ #
+
+
+# ENVIRONMENT SETUP AND DATA UPLOADING =========================================
+
+# Set working directory
+setwd("/Users/setosandoval/Documents/Problem Set 3")
+
+# Clean the environment
+rm(list = ls())
+
+# Seed
+set.seed(202028276)
+
+# Libraries
+library(pacman)
+p_load(tidyverse,     
+       osmdata,       
+       sf,            
+       ggplot2,
+       leaflet)
+
+# Data 
+train <-read.csv("stores/data/raw/train_test/train.csv")
+test <-read.csv("stores/data/raw/train_test/test.csv")
+
+# Combined data
+combined <- rbind(train, test) 
+
+# Only location variables
+combined <- combined %>%
+  select(property_id, lat, lon)
+
+# Data as sf object
+data_sf <- st_as_sf(combined, coords = c("lon", "lat"), crs = 4326) 
+
+
+
+# UPZ ==========================================================================
+
+# Variables: UPZ and Localidad by ID
+
+# UPZ data
+UPZ <- st_read("stores/data/raw/external/UPZ") 
+UPZ <- st_transform(UPZ, crs = st_crs(data_sf))
+
+# Rename variables
+UPZ <- UPZ %>%
+  rename(id_UPZ = codigo_upz, 
+         id_local = codigo_loca)
+
+# Assign localidad ans UPZ to each property
+nearest_UPZ <- st_nearest_feature(data_sf, UPZ) 
+data_sf <- cbind(data_sf, UPZ[nearest_UPZ, c("id_UPZ", "id_local")])
+data_sf <- data_sf[, !names(data_sf) %in% "geometry.1"]
+
