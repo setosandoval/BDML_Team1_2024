dcab64d90a6d6ae8c86d68a6448caadd80adf47c
mafeblanco19 <m.blancoc@uniandes.edu.co>
2024-12-01 23:40:32 -0500

    Amenities

--- Problem Set 3/scripts/02_data_location.R
+++ Problem Set 3/scripts/02_data_location.R
@@ -139,3 +139,101 @@ nearest_M_val <- st_nearest_feature(data_sf, manz_val)
 data_sf <- cbind(data_sf, manz_val[nearest_M_val, c("val_cat", "val_com")]) 
 data_sf <- data_sf[, !names(data_sf) %in% "geometry.1"]
 
+
+
+# AMENITIES OPEN STREET MAP ====================================================
+
+# Variables: Distance to amenities:
+#            1. Park
+#            2. School
+#            3. Highway
+#            4. Supermarket
+#            5. Hospital
+#            6. Public Transport Station (Transmilenio)
+#            7. Restaurant
+#            8. Cicleway
+#            9. Police Station
+#            10. University
+#            11. Bank
+#            12. Mall
+
+# Function to calculate distances to an OSM amenity 
+calculate_amenity_distance <- function(data_sf, bbox, amenity_key, amenity_value, variable_name, amenity_type) {
+  # Query OSM for the specified amenity
+  amenity_data <- opq(bbox = bbox) %>%
+    add_osm_feature(key = amenity_key, value = amenity_value) %>%
+    osmdata_sf()
+  
+  # Select amenities based on type
+  if (amenity_type == "point") {
+    amenity_layer <- amenity_data$osm_points
+  } else if (amenity_type == "polygon") {
+    amenity_layer <- amenity_data$osm_polygons
+  } else if (amenity_type == "line") {
+    amenity_layer <- amenity_data$osm_lines
+  } else {
+    stop("Invalid amenity_type. Use 'point', 'polygon', or 'line'.")
+  }
+  
+  # Ensure CRS alignment
+  data_sf <- st_transform(data_sf, crs = st_crs(amenity_layer))
+  amenity_layer <- st_transform(amenity_layer, crs = st_crs(data_sf))
+  
+  # Calculate distances
+  distances <- st_distance(data_sf, amenity_layer, by_element = FALSE)
+  min_distances <- apply(distances, 1, min)  # Minimum distance for each property
+  
+  # Add the calculated distances as a new variable
+  data_sf <- cbind(data_sf, setNames(data.frame(as.numeric(min_distances)), variable_name))
+  
+  return(data_sf)
+}
+
+# Bounding box for Bogotá
+bbox <- st_bbox(UPZ)
+
+# Distance to parks (polygons)
+data_sf <- calculate_amenity_distance(data_sf, bbox, "leisure", "park", "dist_park", amenity_type = "polygon")
+
+# Distance to schools (points)
+data_sf <- calculate_amenity_distance(data_sf, bbox, "amenity", "school", "dist_school", amenity_type = "point")
+
+# Distance to primary roads (lines)
+data_sf <- calculate_amenity_distance(data_sf, bbox, "highway", c("primary", "secondary"), "dist_highway", amenity_type = "line")
+
+# Distance to supermarkets (points)
+data_sf <- calculate_amenity_distance(data_sf, bbox, "shop", "supermarket", "dist_market", amenity_type = "point")
+
+# Distance to hospitals (points)
+data_sf <- calculate_amenity_distance(data_sf, bbox, "amenity", c("hospital", "clinic"), "dist_hospital", amenity_type = "point")
+
+# Distance to public transport stations (points)
+data_sf <- calculate_amenity_distance(data_sf, bbox, "public_transport", "station", "dist_station", amenity_type = "point")
+
+# Distance to restaurants (points)
+data_sf <- calculate_amenity_distance(data_sf, bbox, "amenity", "restaurant", "dist_rest", amenity_type = "point")
+
+# Distance to cycleways (lines)
+data_sf <- calculate_amenity_distance(data_sf, bbox, "highway", "cycleway", "dist_cycle", amenity_type = "line")
+
+# Distance to police stations (points)
+data_sf <- calculate_amenity_distance(data_sf, bbox, "amenity", "police", "dist_police", amenity_type = "point")
+
+# Distance to univerisities (points)
+data_sf <- calculate_amenity_distance(data_sf, bbox, "amenity", "university", "dist_uni", amenity_type = "point")
+
+# Distance to banks (points)
+data_sf <- calculate_amenity_distance(data_sf, bbox, "amenity", "bank", "dist_bank", amenity_type = "point")
+
+# Distance to malls (points)
+data_sf <- calculate_amenity_distance(data_sf, bbox, "shop", "mall", "dist_mall", amenity_type = "point")
+
+
+
+# SAVE DATA ====================================================================
+
+# Not sf object
+data_final <- st_drop_geometry(data_sf)
+
+# Export data
+write.csv(data_final, "stores/data/raw/train_test/data_location.csv", row.names = F)
